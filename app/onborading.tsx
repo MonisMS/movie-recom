9d3b8fc9311f34b02e77e0e017073a42cbc9b022
Monis <monissms16@gmail.com>
2026-02-24 00:00:53 +0530

    Initial commit

---
+++ app/onborading.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onborading = () => {
+  return (
+    <View>
+      <Text>onborading</Text>
+    </View>
+  )
+}
+
+export default onborading
+
+const styles = StyleSheet.create({})
